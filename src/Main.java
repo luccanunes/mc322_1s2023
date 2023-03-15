b4e0253c1d8ef4dfea9081b8aa221a2f8038bfeb
lucca <luccanunesdemileto@gmail.com>
2023-03-14 22:30:59 -0300

    Testar validacao CPF

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
 public class Main {
-
+    public static void main(String[] args) {
+        Cliente cl = new Cliente("tutu", "059.256.895-97", "07/07/2004", "salvador", 18);
+        System.out.println(cl.getNome());
+        System.out.println(cl.validarCPF());
+    }
 }
