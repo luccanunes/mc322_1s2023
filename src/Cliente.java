16ba2360a3b2f7954326d4007daf2ca03a861799
lucca <luccanunesdemileto@gmail.com>
2023-03-14 22:16:00 -0300

    Implementar validacao de CPF

--- src/Cliente.java
+++ src/Cliente.java
@@ -52,4 +52,36 @@ public class Cliente {
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
+    public boolean validarCPF() {
+        String temp = this.cpf;
+        temp = temp.replaceAll("[\\.-]", "");
+        if (temp.length() != 11)
+            return false;
+        boolean todosIguais = true;
+        for (int i = 1; i < 11; ++i)
+            if (temp.charAt(i) != temp.charAt(0))
+                todosIguais = false;
+        if (todosIguais)
+            return false;
+
+        int primeiroDigito = 0;
+        for (int i = 0; i < 9; ++i) {
+            primeiroDigito += (10 - i) * Character.getNumericValue(temp.charAt(i));
+        }
+        primeiroDigito = 11 - (primeiroDigito % 11);
+        if (primeiroDigito >= 10)
+            primeiroDigito = 0;
+
+        int segundoDigito = 0;
+        for (int i = 0; i < 10; ++i) {
+            segundoDigito += (11 - i) * Character.getNumericValue(temp.charAt(i));
+        }
+        segundoDigito = 11 - (segundoDigito % 11);
+        if (segundoDigito >= 10)
+            segundoDigito = 0;
+
+        return primeiroDigito == Character.getNumericValue(temp.charAt(9)) &&
+                segundoDigito == Character.getNumericValue(temp.charAt(10));
+    }
 }
