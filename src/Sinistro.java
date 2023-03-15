ef392fd8b88193116269d67bc21d1b702050d9a1
lucca <luccanunesdemileto@gmail.com>
2023-03-14 21:42:31 -0300

    Criar getters e setters classe Sinistro

---
+++ src/Sinistro.java
@@ -0,0 +1,34 @@
+public class Sinistro {
+    private String data;
+    private String endereco;
+    private int id;
+
+    public Sinistro(String data, String endereco) {
+        this.data = data;
+        this.endereco = endereco;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
